b516b7dfaa8832cecc1e0083c3dd2be182c25046
Gustavo Herrera <gustavoamnaturalesa@gmail.com>
2025-12-07 09:17:25 -0400

    agregre el archivo agencia_telefonica.cpp

---
+++ agencia_telefonica.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//objetivos:
+//1.Registro de contactos con telefono y nombre
+//2.buscar contacto por inicial del nombre
+//3.editar y cargar contacto
+//4.guardar y cargar desde archivo
+//5.usar recursion para buscar coincidencias
+int main(){
+    printf("Agencia Telefonica De Busqueda Inteligente");
+
+    return 0;
+}
